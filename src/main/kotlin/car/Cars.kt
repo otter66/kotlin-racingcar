cde1f8d213ac1bc7054e8ec03efdc12345b6cdf1
otter <a33397397@gmail.com>
2023-04-05 16:07:55 +0900

    feat(car): 자동차들을 랜덤으로 전진시키능 기능 추가

--- src/main/kotlin/car/Cars.kt
+++ src/main/kotlin/car/Cars.kt
@@ -1,5 +1,17 @@
 package car
 
+import random.RandomGenerator
+import random.RandomGenerator.FORWARD_RANDOM_RANGE
+import rule.GameRule
+
 class Cars(
     val values: List<Car>
-)
+) {
+    fun moveRandomForward() {
+        values.forEach {
+            val randomNumber: Int = RandomGenerator.getRandomNumberInRange(FORWARD_RANDOM_RANGE)
+            val isMove: Boolean = GameRule.isForward(randomNumber) ?: false
+            it.moveForward(isMove)
+        }
+    }
+}
--- src/test/kotlin/car/CarsTest.kt
+++ src/test/kotlin/car/CarsTest.kt
@@ -17,4 +17,22 @@ class CarsTest {
         )
         assertThat(cars.values).isNotEmpty
     }
+
+    @Test
+    fun `자동차들을 랜덤으로 전진시킨다`() {
+        val cars: Cars = Cars(
+            listOf<Car>(
+                Car.from("크롱"), Car.from("하티"), Car.from("글로"), Car.from("부나"),
+                Car.from("코비"), Car.from("앜"), Car.from("로피"), Car.from("루피"),
+                Car.from("톰톰톰"), Car.from("토마스"), Car.from("제이순"), Car.from("제임순"),
+                Car.from("멘들"), Car.from("멘델"), Car.from("유전"), Car.from("멧"),
+                Car.from("좌멘델"), Car.from("우써니"), Car.from("둘리"), Car.from("공실"),
+                Car.from("핑구"), Car.from("핑나"), Car.from("벨벨벨"), Car.from("베르")
+            )
+        )
+        val carsBeforeMoveSum: Int = cars.values.sumOf { it.forwardCount.toInt() }
+        cars.moveRandomForward()
+        val carsAfterMoveSum: Int = cars.values.sumOf { it.forwardCount.toInt() }
+        assertThat(carsBeforeMoveSum).isNotEqualTo(carsAfterMoveSum)
+    }
 }
